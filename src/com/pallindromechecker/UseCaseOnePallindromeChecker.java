f79ec92b9d1dc007c7eeb8a5ef95c41a1125413e
Jayant-129 <agrawaljayant86@gmail.com>
2026-03-03 15:50:35 +0530

    Coded UC1 of PallindromeCheckerApp to Initalize the System

---
+++ src/com/pallindromechecker/UseCaseOnePallindromeChecker.java
@@ -0,0 +1,16 @@
+/*
+ * UC1 - Display a welcome message and app details at startup.
+ * @author - developer
+ * @version - 1.0.0
+ */
+
+package com.pallindromechecker;
+
+public class UseCaseOnePallindromeChecker {
+	
+	public static void main(String args[]) {
+		System.out.println("Welcome to Pallindrome Checker Management Service");
+		System.out.println("version: 1.0.0");
+		System.out.println("System Initialize Successfully");
+	}
+}
